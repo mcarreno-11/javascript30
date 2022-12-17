03c26294d625c89d976b6058323d8298460c0cf4
mcarreno-11 <m-carreno@hotmail.com>
2022-12-17 15:47:12 -0800

    finish day 4

--- 04-Array-Cardio/script.js
+++ 04-Array-Cardio/script.js
@@ -28,17 +28,54 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const fifteen = inventors.filter(function(inventor) {
+    if(inventor.year >= 1500 && inventor.year < 1600) {
+        return true;
+    }
+});
+console.table(fifteen);
+
+    //filter with arrow function
+const fifteenV2 = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+console.table(fifteenV2);
+
+console.log(fifteen[0].first);
+
+
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
 
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullNames); 
+
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const ordered = inventors.sort(function(a,b) {
+if(a.year > b.year){
+  return 1;
+} else {
+  return -1;
+}
+});
+console.log(ordered);
+
+const orderedv2 = inventors.sort((a,b,) => a.year > b.year ? 1 : -1);
+console.log(orderedv2);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const totalYears = inventors.reduce((total, invetor) => {
+  return total + (invetor.passed - invetor.year)
+}, 0)
 
 // 5. Sort the inventors by years lived
+const oldest = inventors.sort(function(a, b) {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  return lastGuy > nextGuy ? -1 : 1;
+});
+console.table(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
@@ -46,8 +83,31 @@ const people = [
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(', ');
+  const [bLast, bFirst] = nextOne.split(', ');
+  return aLast > bLast ? 1 : -1;
+});
+console.log(alpha);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
 
+const transportation = data.reduce(function(obj, item) {
+  //console.log(item);
+  //console.log(obj);
+  // the item is the object of the list 
+
+  //we make an entry for it sens theres no item in the object or we dont know
+  if(!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+
+}, 
+//this is a black object and it will return the object you can add to that object 
+{});
+
+console.log(transportation);
